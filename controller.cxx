3ff407da9dc31cfe9daec5ffab442e53e2768351
ꦌ ꦫꦶ ꦏ꧀ꦦꦿ ꦧ ꦮ ꦑ ꦩ ꦭ꧀ <erik@darapsa.co.id>
2019-09-08 15:45:53 +0800

    Minor type syntax change

--- controller.cxx
+++ controller.cxx
@@ -4,7 +4,7 @@
 
 Controller::Controller(QObject* parent) : QObject{parent}
 {
-	Worker* worker = new Worker;
+	auto worker = new Worker;
 	worker->moveToThread(&thread);
 	connect(&thread, &QThread::finished, worker, &QObject::deleteLater);
 
