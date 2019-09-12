c96cf752bdf51ab412544f8c366209f261416d8c
ꦌ ꦫꦶ ꦏ꧀ꦦꦿ ꦧ ꦮ ꦑ ꦩ ꦭ꧀ <erik@darapsa.co.id>
2019-09-12 12:42:56 +0800

    More consistent function names

--- worker.cxx
+++ worker.cxx
@@ -9,7 +9,8 @@ Worker::Worker()
 void Worker::logIn(QString const& name, QString const& password)
 {
 	rtclient_login(name.toLatin1().constData(), password.toLatin1().constData());
-	rtclient_user(name.toLatin1().constData());
+	auto user = rtclient_userget(name.toLatin1().constData());
+	rtclient_userfree(user);
 }
 
 void Worker::search(QString const& owner)
